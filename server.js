75e8b8038d6c6a01c7f2171b44a3ff5ad5343810
oolga77 <oemi.ua@gmail.com>
2023-06-30 17:51:31 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+const mongoose = require("mongoose");
+
 const app = require('./app')
 
-app.listen(3001, () => {
-  console.log("Server running. Use our API on port: 3001")
+const DB_HOST = "mongodb+srv://Olga:sXGU57yrJUgx0yXM@cluster0.n2quavc.mongodb.net/db-contacts";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3001, () => {
+    console.log("Server running. Use our API on port: 3001")
 })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);  
+  })
+
+
+
+
+
